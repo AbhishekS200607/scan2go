84c554768d46d0885d0309b699b440c352f3c91b
AbhishekS200607 <asn18123@gmail.com>
2026-09-11 23:14:58 +0530

    feat: setup Vercel local dev with .env.local support and vercel-dev script

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,5 +1,7 @@
 const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../../.env.local') });
 require('dotenv').config({ path: path.join(__dirname, '../../.env') });
+require('dotenv').config({ path: path.join(__dirname, '../../../.env.local') });
 require('dotenv').config({ path: path.join(__dirname, '../../../.env') });
 
 module.exports = {
